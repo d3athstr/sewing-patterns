d93922123362855b4326a809cb8b468a37eb0dbc
d3athstr <42899815+d3athstr@users.noreply.github.com>
2025-04-14 20:55:13 -0400

    removed all of the php garbage

--- frontend/dist/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>&copy; <?php echo date("Y"); ?> Sewing Patterns. All rights reserved.</p>
-    <p>Contact us at <a href="mailto:info@garmentgallery.empire12.net">info@garmentgallery.empire12.net</a></p>
-  </footer>
-</body>
-</html>
